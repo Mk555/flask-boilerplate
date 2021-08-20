7e693d2ad110f7e7d33cf9ca495fda07be3c3609
mk555 <duriez.fox@gmail.com>
2021-08-20 11:03:42 +0000

    Improve logging

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import logging
 from app import create_app
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 app = create_app()
 
